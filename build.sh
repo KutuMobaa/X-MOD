44c17c459e672d61102344964ce0985d81f56cd3
KutuMobaa <124151847+KutuMobaa@users.noreply.github.com>
2023-06-09 19:27:46 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
 
+zip -r -o -X -ll X-MOD.Module.Magisk-$(cat module.prop | grep 'version=' | awk -F '=' '{print $2}').zip ./ -x '.git/*' -x 'build.sh' -x 'docs/*' -x 'CHANGELOG.md' -x 'README.md' -x 'update.json' -x '.github/*'
